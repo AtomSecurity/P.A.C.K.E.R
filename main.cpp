7aa356bbab2e0bbd4e2df86e0d950a9da491979d
cr7pt0pl4gu3 <nababkind@protonmail.com>
2021-01-16 21:32:51 +0200

    Added main stub, removed useless test files, updated CMakeLists.txt for build purposes.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#define UNICODE
+#include <iostream>
+#include <Windows.h>
+
+// #include <activator.h>
+// #include <loader.h>
+
+int main()
+{
+    std::cout << "Welcome to P.A.C.K.E.R!";
+    /*
+    if(activateLicense())
+    {
+        loadResource();
+    }
+    else
+    {
+        return 1;
+    }
+    */
+    std::cout << "Goodbye!" << std::endl;
+}

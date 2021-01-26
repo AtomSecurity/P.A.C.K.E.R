6cb110af32aaa04cf48313b7ad9dd26feefcc93f
Nasta_cia <nastya.kalashnikova.108@gmail.com>
2021-01-26 16:41:11 +0200

    Fixed client-server app

--- nasta_cia/client.cpp
+++ nasta_cia/client.cpp
@@ -1,17 +1,22 @@
-#define UNICODE
+//#define UNICODE
+#include "pch.h"
 #include <iostream>
 #include <winsock2.h>
 #include <Ws2tcpip.h>
 #include <cstdlib>
 #include <string>
+#include <fstream>
+#include <iterator>
 
 #pragma comment(lib, "Wsock32.lib")
 #pragma comment(lib, "Ws2_32.lib")
 
+using namespace std;
 
 int main()
 {
-	WORD wVersionRequested {MAKEWORD(2, 2)};
+	
+	WORD wVersionRequested{ MAKEWORD(2, 2) };
 	WSADATA wsaData;
 	WSAStartup(wVersionRequested, &wsaData);
 	struct sockaddr_in peer {};
@@ -26,65 +31,119 @@ int main()
 	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
 
 	//посылаем запрос на открытие соединения
-	int requestStatus {connect(s, (struct sockaddr*) &peer, sizeof(peer))};
-    std::string email {};
-	char secretKey[26];
-	std::cout << "Enter your email to get a key -> ";
-    std::getline (std::cin, email);
-    std::cout << std::endl;
-
-    // for email length passing
-	const unsigned int len {email.length()};
-	const char* emailBuf {(const char*) &len};
+	int requestStatus{ connect(s, (struct sockaddr*) &peer, sizeof(peer)) };
 	
-	int status {send(s, emailBuf, 4, 0)};
+
+	std::string email{0};
+	char secretKey[26];
+
+	int Case;
+	std::cout<<"Choose your operation: 1 - get a key(if you don`t have); 2 - verify"<<std::endl;
+	std::cin >> Case;
+
+	// sending case 
+	int status{ send(s, (char*)&Case, 4, 0) };
 	if (status < 0)
 	{
-	    std::cout << "Send failed, status = " << status << std::endl;
+		std::cout << "Send failed, status = " << status << std::endl;
 		exit(1);
 	}
-
-	if (send(s, email.c_str(), email.length(), 0) < 0)
-	{
-	    std::cout << "Send failed" << std::endl;
-		exit(1);
-	}
-
-
-	//принимаем данные
-	if (recv(s, secretKey, sizeof(secretKey), 0) != 0) {
-        secretKey[25] = '\0';
-	}
-	else
-    {
-	    std::cout << "Could not receive server answer." << std::endl;
-	    exit(1);
-    }
-
-
-
-	std::string userKey{};
 	
-	while (true)
+	//case 1, getting key
+	if (Case == 1)
 	{
-	    std::cout << "Enter your key -> ";
-		std::cin >> userKey;
-		std::cout << std::endl;
 
-		if (userKey == secretKey)
+		std::cout << "Enter your email to get a key -> ";
+		cin.ignore();
+		getline(cin, email);
+
+		// for email length passing
+		const unsigned int len{ email.length() };
+		const char* emailBuf{ (const char*)&len };
+	
+		int status{ send(s, emailBuf, 4, 0) };
+		if (status < 0)
 		{
-		    std::cout << "You are authorized, welcome." << std::endl;
-			break;
+			std::cout << "Send failed, status = " << status << std::endl;
+			exit(1);
+		}
+
+		if (send(s, email.c_str(), email.length(), 0) < 0)
+		{
+			std::cout << "Send failed" << std::endl;
+			exit(1);
+		}
+
+		// принимаем данные
+		if (recv(s, secretKey, sizeof(secretKey), 0) != 0) {
+			secretKey[25] = '\0';
 		}
 		else
 		{
-		    std::cout << "Try again.\n" << std::endl;
+			std::cout << "Could not receive server answer." << std::endl;
+			exit(1);
 		}
+
+		std::cout<<"Your key: "<<secretKey<<std::endl;
 	}
 
+	// case 2, checking key
+	else if (Case == 2)
+	{
+		std::string userKey{};
+
+		std::cout << "Enter your e-mail  -> ";
+		std::cin >> email;
+
+		std::cout << "Enter your key -> ";
+		std::cin >> userKey;
+		std::cout << std::endl;
+
+		// for email length passing
+		const unsigned int len{ email.length() };
+		const char* emailBuf{ (const char*)&len };
+
+
+		int status{ send(s, emailBuf, 4, 0) };
+		if (status < 0)
+		{
+			std::cout << "Send failed, status = " << status << std::endl;
+			exit(1);
+		}
+
+		if (send(s, email.c_str(), email.length(), 0) < 0)
+		{
+			std::cout << "Send failed" << std::endl;
+			exit(1);
+		}
+
+		// key passing
+		if (send(s, userKey.c_str(), 25, 0) < 0)
+		{
+			std::cout << "Send failed" << std::endl;
+			exit(1);
+		}
+		char result;
+
+		// checking
+		if (recv(s, &result, 4, 0) < 0)
+		{
+			std::cout << "Recv failed." << std::endl;
+		}
+
+		if (result == '1')
+		{
+			std::cout << "You are authorized." << std::endl;
+		}
+		else
+		{
+			std::cout << "Try again." << std::endl;
+		}
+		
+	}
 
 	closesocket(s);
-	WSACleanup();
 	
-}
+	WSACleanup();
 
+}
--- nasta_cia/server.cpp
+++ nasta_cia/server.cpp
@@ -1,97 +1,250 @@
-#define UNICODE
+//#define UNICODE
+#include "pch.h"
 #include <random>
 #include <string>
 #include <winsock2.h>
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 #pragma comment(lib,"Wsock32.lib")
 
-std::string generateRandomString(int max_length=25)
+std::string generateRandomString(int max_length = 25)
 {
-    std::string possible_characters {"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    std::random_device rd;
-    std::mt19937 engine(rd());
-    std::uniform_int_distribution<> dist(0, possible_characters.size() - 1);
-    std::string strReturn {};
+	std::string possible_characters{ "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
+	std::random_device rd;
+	std::mt19937 engine(rd());
+	std::uniform_int_distribution<> dist(0, possible_characters.size() - 1);
+	std::string strReturn{};
 
-    for(size_t i {}; i < max_length; ++i)
-    {
-        int random_index {dist(engine)};
-        strReturn += possible_characters[random_index];
-    }
+	for (size_t i{}; i < max_length; ++i)
+	{
+		int random_index{ dist(engine) };
+		strReturn += possible_characters[random_index];
+	}
 
-    return strReturn;
+	return strReturn;
+}
+
+char checking(std::string findName, std::string findPass)
+{
+	std::fstream file("d:\\AtomSecurity\\key_list.txt"); // открыли файл с текстом
+	std::string name, pass;
+	char c;
+
+	while (!file.eof()) {  // прочитали его и заполнили им строку
+		file.get(c);
+		/*if (c == '|')
+		{
+			// проверить, совпадает ли строка с тем, что нужно найти
+			// если совпадает, то продолжать запись в переменную pass
+			if (name == findName)
+			{
+				name.push_back('\0');
+				continue;
+			}
+
+		}
+		else if (c == '\n')
+		{
+			pass.push_back('\0');
+		}
+
+		//если \n, то
+		name.push_back(c);*/
+		int tmp{ 0 };
+		std::string name, pass;
+		while (file >> name >> pass) // reading file line by line
+		{
+			tmp = 0;
+			if (name == findName)
+			{
+				tmp++;
+				std::cout << "Your name was found." << std::endl;
+			}
+			if (pass == findPass)
+			{
+				tmp++;
+				std::cout << "Your password was found." << std::endl;
+			}
+			if (tmp == 2)
+			{
+				std::cout << "You were verified." << std::endl;
+				return '1';
+				break;
+			}
+
+		}
+		if (tmp != 2)
+		{
+			return '0';
+		}
+
+	}
+
+	file.close(); // обязательно закрыли
+
+	/*std::cout << "enter a world for find: ";
+	std::cin >> find;*/
+
+	/*int pos = s.find(find); // поиск
+
+	if (pos == -1)
+	{
+		std::cout << "not finded" << std::endl;
+		return (false);
+	}
+	else
+	{
+		std::cout << "finded in " << pos << " simvol position" << std::endl;
+		return (true);
+	}*/
+
+
 }
 
 int main()
 {
 	std::cout << "Server is running ...\n" << std::endl;
-	WORD wVersionRequested {MAKEWORD(2, 2)};
+	WORD wVersionRequested{ MAKEWORD(2, 2) };
 	WSADATA wsaData;
 	WSAStartup(wVersionRequested, &wsaData);
 
 	// создаем сокет
-	SOCKET s {socket(AF_INET, SOCK_STREAM, 0)};
+	SOCKET s{ socket(AF_INET, SOCK_STREAM, 0) };
 	struct sockaddr_in local {};
 	local.sin_family = AF_INET;
 	local.sin_port = htons(5555);
 	local.sin_addr.s_addr = htonl(INADDR_ANY);
 	// связываем сокет с адресом
-	int bindAddress {bind(s, (struct sockaddr*)&local, sizeof(local))};
+	int bindAddress{ bind(s, (struct sockaddr*)&local, sizeof(local)) };
 	// помещаем сокет в состояние прослушивания
 	listen(s, 5);
 	while (true) {
-		//структура определяет удаленный адрес, с которым соединяется сокет
-		sockaddr_in remote_addr {};
-		int size {sizeof(remote_addr)};
+		// структура определяет удаленный адрес, с которым соединяется сокет
+		sockaddr_in remote_addr{};
+		int size{ sizeof(remote_addr) };
 
-		//инитиялизируем соединение при запросе клиента
-		SOCKET s2 {accept(s, (struct sockaddr*)&remote_addr, &size)};
+		// инитиялизируем соединение при запросе клиента
+		SOCKET s2{ accept(s, (struct sockaddr*)&remote_addr, &size) };
 
-		//принимаем данные
-		char emailBuf[4];
-		int status {recv(s2, emailBuf, 4, 0)};
+		// receiving case 
+		int Case;
+		int status{ recv(s2, (char*)&Case, 4, 0) };
 		if (status < 0)
 		{
-		    std::cout << "Recv failed, status = " << status << std::endl;
+			std::cout << "Recv failed, status = " << status << std::endl;
 			continue;
 		}
-
-		// for email length passing
-		const auto* len_pointer {(const unsigned int*) &emailBuf};
-		unsigned int intemailLen {*len_pointer};
-
-		char* email {new char[intemailLen+1]};
-
-		if (recv(s2, email, static_cast<int>(intemailLen), 0) < 0)
+		
+		//case 1
+		if (Case == 1)
 		{
+			// принимаем данные
+			char emailBuf[4];
+			int status{ recv(s2, emailBuf, 4, 0) };
+			if (status < 0)
+			{
+				std::cout << "Recv failed, status = " << status << std::endl;
+				continue;
+			}
 
-            std::cout << "Recv failed." << std::endl;
-            continue;
+			// for email length passing
+			const auto* len_pointer{ (const unsigned int*)&emailBuf };
+			unsigned int intemailLen{ *len_pointer };
+
+			char* email{ new char[intemailLen + 1] };
+
+
+			if (recv(s2, email, static_cast<int>(intemailLen), 0) < 0)
+			{
+				std::cout << "Recv failed." << std::endl;
+				continue;
+			}
+			email[intemailLen] = '\0';
+			std::cout << "From client: " << email << std::endl;
+
+			// write email to file
+			std::fstream file("D:\\AtomSecurity\\key_list.txt", std::ios::app);
+			file.seekg(0, std::ios_base::end); //Стать в конец файла
+			if (!file)
+			{
+				std::cout << "Error, file is not opened\n\n";
+			}
+			else
+			{
+				std::cout << "File is opened!\n\n";
+			}
+			std::string email2(email, intemailLen);
+			file << email2;
+
+
+			// Cleaning char* email
+			delete[] email;
+			email = nullptr;
+
+			// Random key generation
+			std::string key = generateRandomString();
+
+			std::cout << "Key -> " << key << "\n" << std::endl;
+
+			// Посылает данные на соединенный сокет
+			if (send(s2, key.c_str(), key.length(), 0) < 0)
+			{
+				std::cout << "Send failed." << std::endl;
+				continue;
+			}
+
+			// write key to file
+			file << " " << key << std::endl;
+			file.close();
 		}
-		email[intemailLen] = '\0';
-		std::cout << "From client: " << email << std::endl;
-
-		// Cleaning char* email
-		delete[] email;
-		email = nullptr;
-
-		// Random key generation
-		std::string key = generateRandomString();
-
-		std::cout << "Key -> " << key << "\n" << std::endl;
-
-		// Посылает данные на соединенный сокет
-		if (send(s2, key.c_str(), key.length(), 0) < 0)
+		
+		//case 2
+		else if (Case == 2)
 		{
-		    std::cout << "Send failed." << std::endl;
-            continue;
-		}
+			char emailBuf[4], pass[26];
+			int status{ recv(s2, emailBuf, 4, 0) };
+			if (status < 0)
+			{
+				std::cout << "Recv failed, status = " << status << std::endl;
+				continue;
+			}
+
+			// for email length passing
+			const auto* len_pointer{ (const unsigned int*)&emailBuf };
+			unsigned int intemailLen{ *len_pointer };
+
+			char* email{ new char[intemailLen + 1] };
+
+
+			if (recv(s2, email, static_cast<int>(intemailLen), 0) < 0)
+			{
+				std::cout << "Recv failed." << std::endl;
+				continue;
+			}
+			email[intemailLen] = '\0';
+			std::cout << "From client: email: " << email << std::endl;
 			
-		//}
+			if (recv(s2, pass, 25, 0) < 0)
+			{
+				std::cout << "Recv failed." << std::endl;
+				continue;
+			}
+			pass[25] = '\0';
+			std::cout << "From client: password: " << pass << std::endl;
+
+			char result{ checking(email, pass) };
+
+			if (send(s2, &result, 4, 0) < 0)
+			{
+				std::cout << "Send failed." << std::endl;
+			}
+		}
+
 		closesocket(s2);
 	}
-    // WSACleanup(); Unreachable code
+	// WSACleanup(); Unreachable code
 
 }
+

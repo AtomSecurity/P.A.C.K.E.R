d83e410cb02f8c541448cad2911946ad2cb04523
Nasta_cia <nastya.kalashnikova.108@gmail.com>
2021-02-01 22:30:28 +0200

    Fixed encryption agorithm

--- string-encryption/strencrypt.cpp
+++ string-encryption/strencrypt.cpp
@@ -4,15 +4,9 @@
 std::string stringEncrypt(std::string_view str)
 {
     std::string ret{};
-    std::string numbers {"0123456789"};
 
     for (size_t i{}; i < str.length(); ++i)
     {
-        if (numbers.find(str[i]) != std::string::npos)
-        {
-            ret.push_back(str[i]);
-            continue;
-        }
         ret.push_back(str[i] ^ (i % 5));
     }
 

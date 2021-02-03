94e873c38c79042760fc9eaf423d9277357ad45e
Zver <programm2003ist@gmail.com>
2021-02-03 17:16:02 +0200

    Update RSA code

--- rsa.cpp
+++ rsa.cpp
@@ -1,187 +1,45 @@
 #include <iostream>
-#include <cmath>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
 
-
-bool isPrime(long int prime);
-long int multiplyNumbers(long int p, long int q);
-long int functionOfEueler(long int p, long int q);
-long int calculateE(long int t);
-long int greatestCommonDivisor( long int e, long int t );
-long int calculateD(long int e, long int t);
-long long encrypt(long int i, long int e, long int n);
-long long decrypt(long long i, long int d, long int n);
-
+//https://www.dynamsoft.com/codepool/how-to-use-openssl-generate-rsa-keys-cc.html
+//http://blog.naver.com/PostView.nhn?blogId=ambidext&logNo=221726104308
 
 int main()
 {
-    // Инициализация переменных
-    long int p, q, n, t, e, d, msg;
-    bool flag;
-    long long encryptedText, decryptedText;
+	RSA* key;
+    unsigned char cipher_text[256];
+    unsigned char plain_text_sender[] = "Hello";
+    unsigned char plain_text_receiver[256];
+    unsigned int num;
+    BIGNUM* bne;
+    BIO	*bp_public = nullptr, *bp_private = nullptr;
+    unsigned long e = RSA_F4;
 
 
-    std::cout << "Welcome to RSA program" << std::endl << std::endl;
+    bne = BN_new();
+    BN_set_word(bne, e);
 
-    // Ввод первого простого числа
-    do {
-        std::cout << "Enter a prime number p:" << std::endl;
-        std::cin >> p;
+    key = RSA_new();
 
-        flag = isPrime(p);
+    int ret = RSA_generate_key_ex(key, 2048, bne, nullptr);
 
-        if(!flag)
-        {
-            std::cout << "\nWRONG INPUT (This number is not Prime. A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself)\n" << std::endl;
-        }
+    bp_public = BIO_new_file("public.txt", "w+");
+    ret = PEM_write_bio_RSAPublicKey(bp_public, key);
 
-    } while(!flag);
+    bp_private = BIO_new_file("private.txt", "w+");
+    ret = PEM_write_bio_RSAPrivateKey(bp_private, key, EVP_des_ede3_cbc(), nullptr, 0, nullptr, nullptr);
 
-    // Ввод второго простого числа
-    do {
-        std::cout << "Enter a prime number q:" << std::endl;
-        std::cin >> q;
-
-        flag = isPrime(q);
-
-        if(!flag)
-        {
-            std::cout << "\nWRONG INPUT (This number is not Prime. A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself)\n" << std::endl;
-        }
-
-    } while(!flag);
-
-    // Умножение
-    n = multiplyNumbers(p, q);
-
-    // функция Ейлера
-    t = functionOfEueler(p, q);
-
-    // Расчёт открытой экспоненты
-    e = calculateE(t);
-
-    //Расчёт секретной экспоненты
-    d = calculateD(e, t);
-
-    // Публикация открытого ключа
-    std::cout << "Opening key = {" << e << ", " << n << "}" << std::endl;
-
-    // Ввод сообщения
-    std::cout << "\nEnter a message: " << std::endl;
-    std::cin >> msg;
-
-    // Шифрование сообщения
-    encryptedText = encrypt(msg, e, n);
-    std::cout << "\nEncrypted message: " << encryptedText << std::endl;
-
-    // Расшифровка сообщения
-    decryptedText = decrypt(encryptedText, d, n);
-    std::cout << "\nDecrypted text: " << decryptedText << std::endl;
-
-    return 0;
-}
-
-// Проверка на простоту
-bool isPrime(long int prime)
-{
-    long int i, j;
-
-    j = (long int)sqrt((long double)prime);
-
-    for(i = 2; i <= j; i++)
+    num = RSA_public_encrypt(sizeof(plain_text_sender) - 1, plain_text_sender, cipher_text, key, RSA_PKCS1_PADDING);
+    for(unsigned char i : cipher_text)
     {
-        if(prime % i == 0)
-        {
-            return false;
-        }
+        printf("%x", i);
     }
+    std::cout << std::endl;
 
-    return true;
-}
+    num = RSA_private_decrypt(num, cipher_text, plain_text_receiver, key, RSA_PKCS1_PADDING);
 
-// Умножение
-long int multiplyNumbers(long int p, long int q)
-{
-    return p * q;
-}
+    printf("%s", plain_text_receiver);
 
-// Функция Ейлера
-long int functionOfEueler(long int p, long int q)
-{
-    return (p - 1) * (q - 1);
-}
-
-// Калькулятор открытой экспоненты
-long int calculateE(long int t)
-{
-    long int e;
-
-    for ( e = 2; e < t; e++ )
-    {
-        if (greatestCommonDivisor( e, t ) == 1 )
-        {
-            return e;
-        }
-    }
-
-    return -1;
-}
-
-// Найбольший общий делитель
-long int greatestCommonDivisor( long int e, long int t )
-{
-    while ( e > 0 )
-    {
-        long int myTemp;
-
-        myTemp = e;
-        e = t % e;
-        t = myTemp;
-    }
-
-    return t;
-}
-
-// Калькулятор секретной экспоненты
-long int calculateD( long int e, long int t)
-{
-
-    long int d;
-    long int k = 1;
-
-    while (true)
-    {
-        k = k + t;
-
-        if ( k % e == 0)
-        {
-            d = (k / e);
-            return d;
-        }
-    }
-
-}
-
-// Шифрование
-long long encrypt( long int i, long int e, long int n )
-{
-    long long result = 1;
-    for(int j = 0; j < e; j++)
-    {
-        result = result * i % n;
-    }
-
-    return result;
-
-}
-
-// Расшифровка
-long long decrypt(long long i, long int d, long int n)
-{
-    long long result = 1;
-    for(int j = 0; j < d; j++)
-    {
-        result = result * i % n;
-    }
-
-    return result;
+	return 0;
 }
